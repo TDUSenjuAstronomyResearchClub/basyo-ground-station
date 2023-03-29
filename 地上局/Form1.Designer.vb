9583eca14755bc396ea9abeca7c12bbe0bc942b9
KM <KM@DESKTOP-NI1KGF0>
2023-03-29 23:26:30 +0900

    プロジェクト ファイルを追加します。

---
+++ 地上局/Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        PictureBox5 = New PictureBox()
+        PictureBox6 = New PictureBox()
+        PictureBox7 = New PictureBox()
+        PictureBox8 = New PictureBox()
+        PictureBox9 = New PictureBox()
+        PictureBox10 = New PictureBox()
+        PictureBox11 = New PictureBox()
+        PictureBox12 = New PictureBox()
+        PictureBox13 = New PictureBox()
+        TextBox3 = New TextBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox11, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox12, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox13, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(12, 25)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(256, 159)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 7)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(32, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Time" ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(274, 345)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(386, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(12, 407)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(254, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Location = New Point(274, 25)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(199, 241)
+        PictureBox2.TabIndex = 6
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Location = New Point(479, 25)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(181, 241)
+        PictureBox3.TabIndex = 6
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.Location = New Point(666, 26)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(98, 76)
+        PictureBox4.TabIndex = 7
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.Location = New Point(770, 26)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(98, 76)
+        PictureBox5.TabIndex = 7
+        PictureBox5.TabStop = False
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.Location = New Point(666, 108)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(98, 76)
+        PictureBox6.TabIndex = 7
+        PictureBox6.TabStop = False
+        ' 
+        ' PictureBox7
+        ' 
+        PictureBox7.Location = New Point(770, 108)
+        PictureBox7.Name = "PictureBox7"
+        PictureBox7.Size = New Size(98, 76)
+        PictureBox7.TabIndex = 7
+        PictureBox7.TabStop = False
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.Location = New Point(666, 190)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(98, 76)
+        PictureBox8.TabIndex = 7
+        PictureBox8.TabStop = False
+        ' 
+        ' PictureBox9
+        ' 
+        PictureBox9.Location = New Point(770, 190)
+        PictureBox9.Name = "PictureBox9"
+        PictureBox9.Size = New Size(98, 76)
+        PictureBox9.TabIndex = 7
+        PictureBox9.TabStop = False
+        ' 
+        ' PictureBox10
+        ' 
+        PictureBox10.Location = New Point(666, 272)
+        PictureBox10.Name = "PictureBox10"
+        PictureBox10.Size = New Size(98, 76)
+        PictureBox10.TabIndex = 7
+        PictureBox10.TabStop = False
+        ' 
+        ' PictureBox11
+        ' 
+        PictureBox11.Location = New Point(768, 272)
+        PictureBox11.Name = "PictureBox11"
+        PictureBox11.Size = New Size(98, 76)
+        PictureBox11.TabIndex = 7
+        PictureBox11.TabStop = False
+        ' 
+        ' PictureBox12
+        ' 
+        PictureBox12.Location = New Point(666, 354)
+        PictureBox12.Name = "PictureBox12"
+        PictureBox12.Size = New Size(98, 76)
+        PictureBox12.TabIndex = 7
+        PictureBox12.TabStop = False
+        ' 
+        ' PictureBox13
+        ' 
+        PictureBox13.Location = New Point(770, 354)
+        PictureBox13.Name = "PictureBox13"
+        PictureBox13.Size = New Size(98, 76)
+        PictureBox13.TabIndex = 7
+        PictureBox13.TabStop = False
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(12, 190)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.ReadOnly = True
+        TextBox3.Size = New Size(256, 211)
+        TextBox3.TabIndex = 8
+        TextBox3.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(878, 442)
+        Controls.Add(TextBox3)
+        Controls.Add(PictureBox13)
+        Controls.Add(PictureBox11)
+        Controls.Add(PictureBox9)
+        Controls.Add(PictureBox7)
+        Controls.Add(PictureBox12)
+        Controls.Add(PictureBox10)
+        Controls.Add(PictureBox8)
+        Controls.Add(PictureBox6)
+        Controls.Add(PictureBox5)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "UI"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox11, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox12, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox13, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents PictureBox13 As PictureBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ 地上局/Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.IO.Ports
+Public Class Form1
+    Dim WithEvents myPort As New SerialPort("COM8", 9600)
+
+    Private Sub Form.Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        myPort.Open()
+    End Sub
+
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+
+    End Sub
+
+    Private Sub PictureBox8_Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+
+    End Sub
+
+    Private Sub PictureBox13_Click(sender As Object, e As EventArgs) Handles PictureBox13.Click
+    End Sub
+
+    Private Sub PictureBox11_Click(sender As Object, e As EventArgs) Handles PictureBox11.Click
+    End Sub
+
+    Private Sub PictureBox9_Click(sender As Object, e As EventArgs) Handles PictureBox9.Click
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+    End Sub
+
+    Private Sub PictureBox12_Click(sender As Object, e As EventArgs) Handles PictureBox12.Click
+    End Sub
+
+    Private Sub PictureBox10_Click(sender As Object, e As EventArgs) Handles PictureBox10.Click
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+End Class
